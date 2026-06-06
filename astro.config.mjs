9ae487a43a8c2ddf704dc8bd18b4948f7340a89b
sdldev <sidolapakdev@gmail.com>
2026-06-06 10:36:45 +0700

    fix: add data-cfasync=false to prevent Cloudflare Rocket Loader breaking payment scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
